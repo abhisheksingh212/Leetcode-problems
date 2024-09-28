0233e921eb591dd5097ed558f877ace88e6c0be9
Abhishek Kumar Singh <149613026+HOOPSTER21@users.noreply.github.com>
2024-09-29 01:16:18 +0530

    Time: 19 ms (48.08%), Space: 11.7 MB (69.82%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,7 @@
+from decimal import Decimal as d
+class Solution(object):
+    def multiply(self,num1, num2):
+        n1=int(num1)
+        n2=int(num2)
+        prod=str(n1*n2)
+        return prod
