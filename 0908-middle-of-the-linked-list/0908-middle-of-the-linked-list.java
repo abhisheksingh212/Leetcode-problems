fcf43a356891809f0d1d3c507ecd58d2fc045a9c
Abhishek Kumar Singh <149613026+HOOPSTER21@users.noreply.github.com>
2025-07-16 00:26:14 +0530

    Time: 0 ms (100%), Space: 41.4 MB (32.79%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -9,34 +9,15 @@
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        int n=0;
-        ListNode temp=head;
-        while(temp!=null)
-        {  n++;
-            temp=temp.next;
-        }
-        if(n%2==0)
-        {  temp=head;
-        int i=0;
-            while(temp!=null)
-            { if(i==(n/2))
-            {
-                return temp;
-            }i++;
-                temp=temp.next;
-            }
-        }
-         temp=head;
-        int j=1;
-        while(temp!=null)
-        {
-            if(j==(int)(Math.ceil((double)n / 2)))
-            {
-                return temp;
-            }j++;
-            temp=temp.next;
-        }
-return temp;
+public ListNode middleNode(ListNode head) {
+       //slow fast approach
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       } 
+        
+return slow;
     }
 }
